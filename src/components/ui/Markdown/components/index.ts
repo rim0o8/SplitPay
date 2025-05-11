4c6651b225c31a8fb54632494dbed6eca7e41a62
Yuuri Kurashima <kurashimayuuri@gmail.com>
2025-05-11 21:05:06 +0900

    update

--- src/components/ui/Markdown/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { Blockquote } from './Blockquote';
-export { CodeBlock } from './CodeBlock';
-export { H1, H2, H3 } from './Headings';
-export { InlineCode } from './InlineCode';
-export { Link } from './Link';
-export { OrderedList, UnorderedList } from './Lists';
-export { MermaidDiagram } from './MermaidDiagram';
-export { Table } from './Table';
-
